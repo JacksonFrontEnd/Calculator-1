543e7a7a475e299f731050c187bf30fee935fa64
isushkevich <ivan.sushik@gmail.com>
2021-10-19 20:41:55 +0300

    add number input and clear

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 "use strict";
+
+const outputTop = document.getElementsByClassName("output-top")[0];
+
+const outputBottom = document.getElementsByClassName("output-bottom")[0];
+
+const numbers = document.getElementsByClassName("number");
+
+const ac = document.getElementById("ac");
+
+ac.addEventListener("click", () => clearOutput());
+
+const clearOutput = () => {
+    outputBottom.innerText = "0";
+}
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener("click", e => handleNumberClick(e));
+}
+
+const handleNumberClick = (e) => {
+    updateInputBottom(e.target.innerText);
+}
+
+const updateInputBottom = (number) => {
+    if (number === ",") {
+        if (outputBottom.innerText === "0") {
+            outputBottom.innerText = "0,";
+        } else if (!outputBottom.innerText.includes(",")) {
+            outputBottom.innerText += ",";
+        }
+    } else {
+        if (outputBottom.innerText === "0") {
+            outputBottom.innerText = number;
+        } else {
+            outputBottom.innerText += number;
+        }
+    }
+}
