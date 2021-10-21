abde056d381081d7a98b9231c32ba77f7538bccd
isushkevich <ivan.sushik@gmail.com>
2021-10-21 11:16:35 +0300

    remove top output field and add basic calculations through eval (temporarily)

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
 "use strict";
 
-const outputTop = document.getElementsByClassName("output-top")[0];
-
-const outputBottom = document.getElementsByClassName("output-bottom")[0];
-
+// numbers and ","
 const numbers = document.getElementsByClassName("number");
 
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener("click", e => updateOutput(e.target));
+}
+
+// operations
+const operations = document.getElementsByClassName("orange");
+
+for (let i = 0; i < operations.length; i++) {
+    operations[i].addEventListener("click", e => updateOutput(e.target));
+}
+
+// output and expression storage
+const outputField = document.getElementById("output");
+outputField.value = 0;
+// let expressionStorage;
+
+const updateOutput = (input) => {
+    if (input.dataset.operation === "=") {
+        outputField.value = calculate(outputField.value);
+
+    } else if (input.dataset.operation) {
+        if (outputField.value) {
+            outputField.value += " " + input.dataset.operation + " ";
+        }
+
+    } else {
+        if (outputField.value === "0") {
+            outputField.value = "";
+        }
+
+        outputField.value += input.innerText;
+    }
+
+    // expressionStorage = outputField.value.split(" ");
+}
+
+// calculating result
+const calculate = (str) => {
+    return eval(str + "+0");
+}
+
+// clear function
 const ac = document.getElementById("ac");
 
-ac.addEventListener("click", () => clearOutput());
-
-const clearOutput = () => {
-    outputBottom.innerText = "0";
-}
-
-for (let i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener("click", e => handleNumberClick(e));
-}
-
-const handleNumberClick = (e) => {
-    updateInputBottom(e.target.innerText);
-}
-
-const updateInputBottom = (number) => {
-    if (number === ",") {
-        if (outputBottom.innerText === "0") {
-            outputBottom.innerText = "0,";
-        } else if (!outputBottom.innerText.includes(",")) {
-            outputBottom.innerText += ",";
-        }
-    } else {
-        if (outputBottom.innerText === "0") {
-            outputBottom.innerText = number;
-        } else {
-            outputBottom.innerText += number;
-        }
-    }
-}
+ac.addEventListener("click", () => {
+    outputField.value = 0;
+    // expressionStorage = [];
+})
