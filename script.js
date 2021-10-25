87e43582245a6dfe3bd51852dc078c074febfdaa
isushkevich <ivan.sushik@gmail.com>
2021-10-25 14:10:29 +0300

    add keyboard input and rewrite the updateOutput function

--- script.js
+++ script.js
@@ -4,45 +4,85 @@
 const numbers = document.getElementsByClassName("number");
 
 for (let i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener("click", e => updateOutput(e.target));
+    numbers[i].addEventListener("click", e => updateOutput(e));
 }
+
 
 // operations
 const operations = document.getElementsByClassName("operation");
 
 for (let i = 0; i < operations.length; i++) {
-    operations[i].addEventListener("click", e => updateOutput(e.target));
+    operations[i].addEventListener("click", e => updateOutput(e));
 }
+
+
+// keyboard input
+document.addEventListener("keydown", (e) => {
+    updateOutput(e);
+});
+
 
 // output and expression storage
 const outputField = document.getElementById("output");
 outputField.value = 0;
 // let expressionStorage;
 
+
 const updateOutput = (input) => {
-    if (input.dataset.operation === "=") {
-        outputField.value = calculate(outputField.value);
+    let inputParsed; // parsing input
 
-    } else if (input.dataset.operation) {
-        if (outputField.value) {
-            outputField.value += " " + input.dataset.operation + " ";
-        }
+    if (input.key) { // keyboard input
+        inputParsed = input.key;
+    } else { // mouse clicks
+        inputParsed = input.target.dataset.value;
+    }
 
-    } else {
+    const addDigit = (digit) => {
         if (outputField.value === "0") {
             outputField.value = "";
         }
+        outputField.value += digit;
+    }
 
-        outputField.value += input.innerText;
+    const addOperation = operation => outputField.value += " " + operation + " ";
+
+    const equals = () => outputField.value = calculate(outputField.value);
+
+    console.log(inputParsed);
+
+    switch (inputParsed) {
+        case "=":
+            equals();
+            break;
+        case "+":
+        case "-":
+        case "/":
+        case "*":
+            addOperation(inputParsed);
+            break;
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            addDigit(inputParsed);
+            break;
     }
 
     // expressionStorage = outputField.value.split(" ");
 }
 
+
 // calculating result
 const calculate = (str) => {
     return eval(str);
 }
+
 
 // clear function
 const ac = document.getElementById("ac");
@@ -52,9 +92,8 @@ ac.addEventListener("click", () => {
     // expressionStorage = [];
 })
 
-//theme switching
-// element.className = element.className.replace(/container-1/, 'container-2')
 
+//theme switching
 const switchTheme = document.getElementById("switch-theme");
 
 switchTheme.addEventListener("click", () => {
